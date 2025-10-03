3d8753be3c526773ada2b279fbef57694ba375ce
ronesanttos <ronesanttos17@gmailcom>
2025-10-03 19:51:45 -0300

    corrigindo render

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, etc.)
+pip install -r requirements.txt
+
+# Convert static asset files
+python manage.py collectstatic --noinput
+
+# Apply any outstanding database migrations
+python manage.py migrate
