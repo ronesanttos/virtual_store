3d33e67899b72fbadb41c929940015b831f6b72a
ronesanttos <ronesanttos17@gmailcom>
2026-01-19 21:05:53 -0300

    Alteracoes no banco de dados

--- project/settings.py
+++ project/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
         ssl_require=True  
     )
